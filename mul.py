84b34e8daf1825d7903399aa3c4ac12839c8b151
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-12 21:58:39 +0900

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,8 +1,8 @@
-def mul(price,number):
+def mul(price,number): #あらかじめ宣言
     result = price * number
     return result
 
 price = 200
 number = 3
 
-print(mul(price,number))
+print(mul(price,number)) #関数mulを呼び出し
