62b31f17159f1a1f1b4c5dc8843e7a6a36ebb887
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-13 22:26:21 +0900

    Delete format1.py

--- format/format1.py
+++
@@ -1,7 +0,0 @@
-name = "Shinji"
-age = "21"
-
-print("{} is {} years old".format(name,age)) #穴の数だけ format の引数が増える
-
-print("{a} is {b} years old" .format(b = age,a = name)) 
-#穴埋めの順番を指定やキーワードによる引数を使用することも可能
