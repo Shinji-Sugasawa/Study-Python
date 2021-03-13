140aea5dfb3fa78698f9a832f7dd967788156f61
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-13 23:22:41 +0900

    Update dict-add.py

--- Chapter3/dictionaly/dict-add.py
+++ Chapter3/dictionaly/dict-add.py
@@ -5,3 +5,6 @@ print(user['name'])#terada
 
 user['tell'] = '123-4566-8901'
 print(user)#{'name': 'terada', 'age': 21, 'tell': '123-4566-8901'}
+
+print(int(user['age'])+1)#22 ←listは数値を入れてもstrで返される
+print(user.get('age')+3)#get()メソッドを使えばintで返される
