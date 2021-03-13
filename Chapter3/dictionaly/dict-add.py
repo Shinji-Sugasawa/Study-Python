536485d2b70dd93bf46ae444c529c7f0c13c0a13
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-13 23:14:49 +0900

    Create dict-add.py

---
+++ Chapter3/dictionaly/dict-add.py
@@ -0,0 +1,7 @@
+user = {'name':'Shinji','age':21}
+
+user['name'] = 'terada'
+print(user['name'])#terada
+
+user['tell'] = '123-4566-8901'
+print(user)#{'name': 'terada', 'age': 21, 'tell': '123-4566-8901'}
