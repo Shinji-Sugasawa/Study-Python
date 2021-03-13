76add0cb8ee9ec0a0d29e63957c77def6e5cdca6
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-13 22:26:32 +0900

    Delete int-str.py

--- int-str.py
+++
@@ -1,9 +0,0 @@
-age = 20 
-print("int → str")
-#print("I\'m "+age+" old.") 変数 age が int 型のため実行不可
-print("I\'m "+str(age)+" years old.")
-
-age = "20"
-print("str → int")
-#print(age + 1) 変数 age が str 型のため実行不可
-print(int(age)+1)
