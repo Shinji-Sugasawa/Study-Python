25c346ee042d37249a752b6daff000a95fae1405
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-14 23:16:57 +0900

    Create bool.py

---
+++ Chapter3/if-else/bool.py
@@ -0,0 +1,13 @@
+print(bool(20))#true
+
+print(bool('abc'))#true
+
+print(bool(0))#false
+
+#Falseになる条件
+print('以下はすべてFalseになる')
+
+print(bool(0.0))
+print(bool([]))
+print(bool(False))
+print(bool(None))
