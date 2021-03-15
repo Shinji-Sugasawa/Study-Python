9677fa306bbc530b04c78a3b9d676f6e502dafb8
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-15 21:04:00 +0900

    Update set.py

--- Chapter3/container/set.py
+++ Chapter3/container/set.py
@@ -6,3 +6,7 @@ print(my_pets)#dogが重複しているため、出力は{'cat', 'dog'}
 print(my_pets | friend_pets)#和集合 {'dog', 'hamster', 'cat'}
 
 print(my_pets & friend_pets)#積集合 {'cat'}
+
+my_pets.add("bird")#set型はデータの更新ができる
+my_pets.add("fish")
+print(my_pets)
