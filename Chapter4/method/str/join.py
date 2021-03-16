8fe874f1906fe48ca746cecdca194af480a6caf0
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-16 23:19:23 +0900

    Update join.py

--- Chapter4/method/str/join.py
+++ Chapter4/method/str/join.py
@@ -1,4 +1,6 @@
-message = ["Hello","Nice","to","meet","you","."]
+message = ["Nice","to","meet","you","."]
 
 print(" ".join(message))
-
+#引数に結合したい文字列が格納されたリストをいれ、
+# 「.」のまえに連結させるものを入れる
+#結合文字列　=　連結文字列.join(結合させたい文字列が格納されているリスト)
