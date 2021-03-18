71efd78088619949a4e32e2eeea1d82b0a8702e5
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-19 00:19:54 +0900

    Update main4.py

--- Chapter4/practice/main4.py
+++ Chapter4/practice/main4.py
@@ -1,5 +1,12 @@
 data = ["3-A","2-B","1-C","3-D","1-E","3-F","1-G","2-H","3-I"]
 
+counter = {}
+
 for value in data:
     year , name = value.split("-")
-    print(year,name)
+    
+    if year not in counter:
+        counter[year] = []
+    
+    counter[year].append(name)
+print(counter)
