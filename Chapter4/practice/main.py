60aaaaba457b3a1ffe87d8325d1c52da001b1b7d
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-18 23:32:14 +0900

    Update main.py

--- Chapter4/practice/main.py
+++ Chapter4/practice/main.py
@@ -1,6 +1,12 @@
-text = "I am learning Pyton language"
+text = "I am learning Pyton programming language"
 
 split_text = text.split(" ")
 #単語をスペースで分割
 print(split_text)
 #分割結果を出力
+
+counter = 0
+for word in split_text:#forループで分割した文字列を処理
+    counter += 1 #文字をカウント
+
+print(f"単語の数:{counter}")#画面に出力する
