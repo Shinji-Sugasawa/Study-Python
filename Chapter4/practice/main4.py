120ea2f6187e6db9917b7e421e70fba21a94e020
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-19 00:36:48 +0900

    Update main4.py

--- Chapter4/practice/main4.py
+++ Chapter4/practice/main4.py
@@ -9,4 +9,13 @@ for value in data:
         counter[year] = []
     
     counter[year].append(name)
-print(counter)
+    
+for year in sorted(counter):
+#counterという辞書のキーをsorted関数でソートする
+#１つずつ取り出す
+    print(f"{year}年")
+    for order , name in enumerate(counter[year]):
+#enumerateはインデックス番号、要素の順で取得される
+#orderにindex値、nameにyearキーの要素を代入
+        print(f"{order+1}位 : {name}")
+    print()
