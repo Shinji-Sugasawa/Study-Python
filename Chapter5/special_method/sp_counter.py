0c722179820eba51af6ecddae693d50b169f7401
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-21 00:05:05 +0900

    Update sp_counter.py

--- Chapter5/special_method/sp_counter.py
+++ Chapter5/special_method/sp_counter.py
@@ -1,6 +1,12 @@
-class Count:
-    def __init__(self, initial = 0):
+class Counter:
+    def __init__(self, initial = 0):#コンストラクタ(スペシャルメソッド)
         self.value = initial    #initial引数をvalueの初期値として使う
     
-    def count_up(self):
+    def count_up(self):#通常のメソッド
         self.value +=1
+        
+    def __str__(self):#文字列表現を返すメソッド
+        return f"Counter({self.value})"
+    
+    def __add__(self,other):# +演算子と呼ばれるスペシャルメソッド
+        return Counter(self.value + other.value)#Counterを新たに生成して返す
