a57c766f15ad8cf6512bee890d4e45ce0b93fec6
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-21 00:30:32 +0900

    Update sp_counter.py

--- Chapter5/special_method/sp_counter.py
+++ Chapter5/special_method/sp_counter.py
@@ -10,3 +10,16 @@ class Counter:
     
     def __add__(self,other):# +演算子と呼ばれるスペシャルメソッド
         return Counter(self.value + other.value)#Counterを新たに生成して返す
+    
+    
+    #"""
+    c1 = Counter(10)    #コンストラクタ(__init__メソッド)がよばれている
+    c1.count_up()#count_upメソッドを呼び出している
+    str(c1)#文字列表現(__str__メソッド)が呼び出されている
+    #'Counter(11)'が出力
+    c2 = Counter(1)
+    c3 = c1 + c2 #通常の+演算子で計算
+    str(c3)#'Counter(12)'が出力
+    
+    c4 = c1.__add__(c2)#c1 + c2と同じ意味
+    #"""
