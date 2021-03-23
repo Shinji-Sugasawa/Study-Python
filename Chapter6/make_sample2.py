9a51703ed5e3d0196d90f5e480212544b2fb7901
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-23 22:55:42 +0900

    Update make_sample2.py

--- Chapter6/make_sample2.py
+++ Chapter6/make_sample2.py
@@ -2,7 +2,6 @@ with open("sample2.txt","w",encoding ="utf-8") as f:
         f.write("私の名前は鈴木太郎です。\n")
         f.write("私の職業は、学生です。")
 
-"""
-with open("sample.txt","r",encoding = "utf-8") as f:
+
+with open("D:\ドキュメント\GitHub\Study-Python\Chapter6\sample.txt","r",encoding = "utf-8") as f:
         f.read()
-"""
