115f1f9fae0e82a345fd8d1d8343fa104109fb73
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-23 23:48:15 +0900

    Update make_sample2.py

--- Chapter6/make_sample2.py
+++ Chapter6/make_sample2.py
@@ -4,4 +4,4 @@ with open("sample2.txt","w",encoding ="utf-8") as f:
 
 
 with open("D:\Documents\GitHub\Study-Python\Chapter6\sample2.txt","r",encoding = "utf-8") as f:
-        f.read()
+        print(f.read())
