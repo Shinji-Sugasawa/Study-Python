49e66cef31882d8b5765a57f5a92bd3fcabf842f
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-23 23:38:33 +0900

    Update make_sample2.py

--- Chapter6/make_sample2.py
+++ Chapter6/make_sample2.py
@@ -3,5 +3,5 @@ with open("sample2.txt","w",encoding ="utf-8") as f:
         f.write("私の職業は、学生です。")
 
 
-with open("D:\ドキュメント\GitHub\Study-Python\Chapter6\sample.txt","r",encoding = "utf-8") as f:
+with open("D:\Documents\GitHub\Study-Python\Chapter6\sample.txt","r",encoding = "utf-8") as f:
         f.read()
