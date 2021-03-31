2a44daca3ebb6c7de29bf35e935644108f7077d3
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-31 22:58:29 +0900

    Update make_qr.py

--- make_QR/make_qr.py
+++ make_QR/make_qr.py
@@ -2,9 +2,9 @@ import qrcode   #QRコード生成用のパッケージをインポートする
 import qrcode.image.svg #QRコードをSVG画像で保存するモジュールをインポートする
 
 class MakeQR:   #クラス定義
-    def __init__(self): #コンストラクタを定義
-        self.url = "http://www.nao.net.it-chiba.ac.jp"  #URLを直接定義する
-        self.output = "python.svg"  #アウトプットファイル名を直接定義する
+    def __init__(self, output): #コンストラクタを定義
+        self.url = ""  #URLの初期値を空にする
+        self.output = output  #アウトプットファイル名を直接定義する
     
     def make(self): #makeメソッドの定義
         factory = qrcode.image.svg.SvgImage
