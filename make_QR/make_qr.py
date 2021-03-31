ef42c8bc1dfeb3c266ce4d929a75c10eb8b3acb6
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-03-31 22:43:19 +0900

    Update make_qr.py

--- make_QR/make_qr.py
+++ make_QR/make_qr.py
@@ -1,3 +1,6 @@
 import qrcode   #QRコード生成用のパッケージをインポートする
 import qrcode.image.svg #QRコードをSVG画像で保存するモジュールをインポートする
 
+class MakeQR:   #クラス定義
+    def __init__(self): #コンストラクタを定義
+        self.url = "http://www.nao.net.it-chiba.ac.jp"
