aca31d94701dedb3a745c463c0af393aacb07097
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-04-01 23:30:28 +0900

    Update gcd1.py

--- Euclid/gcd1.py
+++ Euclid/gcd1.py
@@ -13,3 +13,14 @@ def gcd(x,y):
         if count % y == 0:
             y_list.append(count)
         count +=1
+        
+    count_x,count_y = 0,0
+    gcd_list = []
+    for count_x in list_x:
+        for count_y in y_list:
+            if cont_x == count_y:
+                gcd_list.append(count_x)
+    
+    return max(gcd_list)
+
+print(gcd(12,18))
