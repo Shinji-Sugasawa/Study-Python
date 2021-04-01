7cca306554447dd7778d09c73a931430b78ff247
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-04-02 00:07:24 +0900

    Update euclid.py

--- Euclid/euclid.py
+++ Euclid/euclid.py
@@ -1,10 +1,11 @@
-def gcd(x,y):
-    if y == 0:
-        return x
-    else:
-        return gcd(y,x % y)
+def gcd(x,y):   #ユークリッドを求める関数
+    if y == 0:  #ｙの値が0ならば
+        return x    #xの値を返す
+    else:   #yの値が0以外ならば
+        return gcd(y,x % y) #再起を用いる
 
 get_x = input("1つ目の数値を代入してください:")#1つ目の数値をget_xに代入
 get_y = input("2つ目の数値を代入してください:")#2つ目の数値をget_yに代入
 
-print(gcd(get_x,get_y))
+
+print(gcd(int(get_x),int(get_y)))   #gcdを呼び出す
