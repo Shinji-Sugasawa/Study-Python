a04b8b803477a4310960b16dade431a20b720c54
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2021-04-02 00:00:15 +0900

    Update euclid.py

--- Euclid/euclid.py
+++ Euclid/euclid.py
@@ -4,4 +4,7 @@ def gcd(x,y):
     else:
         return gcd(y,x % y)
 
-print(gcd(12,18))
+get_x = input("1つ目の数値を代入してください:")#1つ目の数値をget_xに代入
+get_y = input("2つ目の数値を代入してください:")#2つ目の数値をget_yに代入
+
+print(gcd(get_x,get_y))
